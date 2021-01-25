551c57607c72f101694b433ce2337fd46b9eaa91
JINJing <38675908+jingjin25@users.noreply.github.com>
2021-01-25 15:36:37 +0800

    Add files via upload

--- utils/dataset.py
+++ utils/dataset.py
@@ -66,6 +66,63 @@ class TrainDataFromHdf5(data.Dataset):
         return self.img_HR.shape[0]
 
 
+class TrainDataFromHdf5_LF(data.Dataset):
+    def __init__(self, file_path, scale, patch_size, an):
+        super(TrainDataFromHdf5_LF, self).__init__()
+
+        hf = h5py.File(file_path)
+        self.img_HR = hf.get('img_HR')  # [N,ah,aw,h,w]
+        self.img_LR = hf.get('img_LR_{}'.format(scale))  # [N,ah,aw,h/s,w/s]
+        self.img_size = hf.get('img_size')  # [N,2]
+
+        self.scale = scale
+        self.psize = patch_size
+        self.an = an
+
+    def __getitem__(self, index):
+
+        # get one item
+        hr = self.img_HR[index]  # [ah,aw,h,w]
+        lr = self.img_LR[index]  # [ah,aw,h/s,w/s]
+
+        # crop to patch
+        H, W = self.img_size[index]
+
+        x = random.randrange(0, H - self.psize, 8)
+        y = random.randrange(0, W - self.psize, 8)
+        hr = hr[:, :, x:x + self.psize, y:y + self.psize]  # [ah,aw,ph,pw]
+        lr = lr[:, :, x // self.scale:x // self.scale + self.psize // self.scale,
+             y // self.scale:y // self.scale + self.psize // self.scale]  # [ah,aw,ph/s,pw/s]
+
+        # 4D augmentation
+        # flip
+        if np.random.rand(1) > 0.5:
+            hr = np.flip(np.flip(hr, 0), 2)
+            lr = np.flip(np.flip(lr, 0), 2)
+
+        if np.random.rand(1) > 0.5:
+            hr = np.flip(np.flip(hr, 1), 3)
+            lr = np.flip(np.flip(lr, 1), 3)
+
+        # rotate
+        r_ang = np.random.randint(1, 5)
+        hr = np.rot90(hr, r_ang, (2, 3))
+        hr = np.rot90(hr, r_ang, (0, 1))
+        lr = np.rot90(lr, r_ang, (2, 3))
+        lr = np.rot90(lr, r_ang, (0, 1))
+
+        # to tensor
+        hr = hr.reshape(-1, self.psize, self.psize)  # [an,ph,pw]
+        lr = lr.reshape(-1, self.psize // self.scale, self.psize // self.scale)  # [an,phs,pws]
+
+        hr = torch.from_numpy(hr.astype(np.float32) / 255.0)
+        lr = torch.from_numpy(lr.astype(np.float32) / 255.0)
+
+        return hr, lr
+
+    def __len__(self):
+        return self.img_HR.shape[0]
+
 class TestDataFromHdf5(data.Dataset):
     def __init__(self, file_path, scale):
         super(TestDataFromHdf5, self).__init__()
